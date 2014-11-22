898e6a97a3d28cb998006594153e7c09087ec76c
Evisceration <eviscerationls@gmail.com>
2014-11-22 12:04:23 +0000

    (2/2) Settings: allow to configure the long press back kill delay
    PureNexus Edits:
    *forward ported to 6.0/7.0
    *modified for new nougat seekbar
    *allow to set in intervals of 250ms up to 3000ms

--- src/com/pure/settings/fragments/NavBarSettings.java
+++ src/com/pure/settings/fragments/NavBarSettings.java
@@ -16,26 +16,55 @@
 
 package com.pure.settings.fragments;
 
+import android.content.ContentResolver;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-public class NavBarSettings extends SettingsPreferenceFragment {
+import com.pure.settings.preferences.CustomSeekBarPreference;
+
+public class NavBarSettings extends SettingsPreferenceFragment
+         implements OnPreferenceChangeListener {
+
+    private static final String LONG_PRESS_KILL_DELAY = "long_press_kill_delay";
+
+    private CustomSeekBarPreference mLongpressKillDelay;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.navbar_settings);
         PreferenceScreen prefScreen = getPreferenceScreen();
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        mLongpressKillDelay = (CustomSeekBarPreference) findPreference(LONG_PRESS_KILL_DELAY);
+        int killconf = Settings.System.getInt(resolver,
+                Settings.System.LONG_PRESS_KILL_DELAY, 1000);
+        mLongpressKillDelay.setValue(killconf);
+        mLongpressKillDelay.setOnPreferenceChangeListener(this);
     }
 
     @Override
     protected int getMetricsCategory() {
         return MetricsEvent.PURE;
+    }
+
+     @Override
+     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mLongpressKillDelay) {
+            int killconf = (Integer) newValue;
+            Settings.System.putInt(resolver, Settings.System.LONG_PRESS_KILL_DELAY, killconf);
+            return true;
+        }
+        return false;
     }
 }
 
