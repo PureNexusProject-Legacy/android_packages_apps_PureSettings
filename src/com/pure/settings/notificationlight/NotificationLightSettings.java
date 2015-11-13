79144edd6a675b6922e3df0b89222d9d4a6173cc
AdrianDC <radian.dc@gmail.com>
2015-11-13 11:47:50 -0500

    Lights with Screen On [2/2]: Optional allowment of lights
    Implement a setting allowing lights to be activated
    for new notifications even if the screen is on.
    
    Lights with screen on and Custom values are separated
    in an advanced section for a cleaner overview.
    
    This setting gives the user an oportunity to activate lights
    with the screen on and also during DayDream screensaver.
    The option is not activated by default.
    
    Changes include :
      frameworks/base
      packages/apps/Settings

--- src/com/pure/settings/notificationlight/NotificationLightSettings.java
+++ src/com/pure/settings/notificationlight/NotificationLightSettings.java
@@ -75,6 +75,7 @@ public class NotificationLightSettings extends SettingsPreferenceFragment implem
     private PreferenceGroup mApplicationPrefList;
     private SystemSettingSwitchPreference mEnabledPref;
     private SystemSettingSwitchPreference mCustomEnabledPref;
+    private SystemSettingSwitchPreference mScreenOnLightsPref;
     private ApplicationLightPreference mDefaultPref;
     private Menu mMenu;
     private PackageListAdapter mPackageAdapter;
@@ -119,6 +120,10 @@ public class NotificationLightSettings extends SettingsPreferenceFragment implem
         mCustomEnabledPref = (SystemSettingSwitchPreference)
                 findPreference(Settings.System.NOTIFICATION_LIGHT_PULSE_CUSTOM_ENABLE);
         mCustomEnabledPref.setOnPreferenceChangeListener(this);
+
+        mScreenOnLightsPref = (SystemSettingSwitchPreference)
+                findPreference(Settings.System.NOTIFICATION_LIGHT_SCREEN_ON);
+        mScreenOnLightsPref.setOnPreferenceChangeListener(this);
 
         mApplicationPrefList = (PreferenceGroup) findPreference("applications_list");
         mApplicationPrefList.setOrderingAsAdded(false);
@@ -333,7 +338,8 @@ public class NotificationLightSettings extends SettingsPreferenceFragment implem
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-        if (preference == mEnabledPref || preference == mCustomEnabledPref) {
+        if (preference == mEnabledPref || preference == mCustomEnabledPref ||
+                preference == mScreenOnLightsPref) {
             getActivity().invalidateOptionsMenu();
         } else {
             ApplicationLightPreference lightPref = (ApplicationLightPreference) preference;
